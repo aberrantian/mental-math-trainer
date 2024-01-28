970d80ae529f13f64d1a6b14aa1836438f4f9534
aberrantian <aberrantian@gmail.com>
2024-01-28 12:57:29 +0000

    track expeval

---
+++ scripts/expeval.js
@@ -0,0 +1,28 @@
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+export function expEval(exp) {
+  console.log(exp);
+  let result;
+
+    for (let index = 0; index < exp.length; index++) {
+      if (exp[index] === "*") {
+
+      }
+    }
+
+  return result;
+}
