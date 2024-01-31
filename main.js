d11be0aed8bf7f3aaa692f7696180007a856fdb4
aberrantian <aberrantian@gmail.com>
2024-01-31 23:36:03 +0000

    add warning

--- main.js
+++ main.js
@@ -7,3 +7,8 @@ let answer = expEval(exp);
 
 const EXP_DIV = document.getElementById("expression");
 EXP_DIV.innerText = exp.join(" ") + " = " + answer;
+
+const WARNING = document.createElement("div");
+WARNING.innerText = "This version of the project is incomplete!";
+WARNING.style = "font-size: 0.8rem;font-weight: bold;position: absolute;top: 0px;background-color: red; width: 100%;"
+document.body.append(WARNING);
