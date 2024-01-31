57626815d25b21ce285cf04ae9771aeaa542c8bc
aberrantian <aberrantian@gmail.com>
2024-01-31 21:20:05 +0000

    create operate function

--- scripts/expeval.js
+++ scripts/expeval.js
@@ -23,6 +23,29 @@ function cloneArray(array) {
 
   return new_array;
 }
+
+function operate(operand_a, operator, operand_b) {
+  let result;
+
+  switch (operator) {
+    case "*":
+      result = multiply(operand_a, operand_b);
+      break;
+    case "/":
+      result = divide(operand_a, operand_b);
+      break;
+    case "+":
+      result = add(operand_a, operand_b);
+      break;
+    case "-":
+      result = subtract(operand_a, operand_b);
+      break;
+    default:
+      break;
+  }
+
+  return result;
+}
   let result;
 
     for (let index = 0; index < exp.length; index++) {
