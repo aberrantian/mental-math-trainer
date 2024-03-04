335e7dc9f451681b082a1836f23478368c3b38ab
aberrantian <aberrantian@gmail.com>
2024-03-04 16:49:14 +0000

    add github link

--- main.js
+++ main.js
@@ -11,6 +11,14 @@ EXP_DIV.innerText = exp.join(" ") + " = ";
 
 const USER_INPUT = document.getElementById("user-input");
 
+const GITHUB = document.createElement("a");
+GITHUB.innerText = "GitHub";
+GITHUB.style = "font-size: 0.8rem;font-weight: bold;position: absolute;top: 0px; width: 100%; color: white; text-align: center;"
+GITHUB.href = "https://github.com/aberrantian/mmt";
+GITHUB.referrerPolicy = "no-referrer";
+GITHUB.target = "_blank";
+document.body.append(GITHUB);
+
 
 function newExp() {
     console.clear();
