1f08aebd6a4520af68801aecb609a301c81394c2
aberrantian <aberrantian@gmail.com>
2024-08-01 20:02:11 +0100

    display expression

--- main.js
+++ main.js
@@ -5,20 +5,10 @@ import { inputHandler } from "./scripts/inputhandler.js";
 let exp = expGen();
 let answer = expEval(exp);
 
-
 const EXP_DIV = document.getElementById("expression");
-EXP_DIV.innerText = exp.join(" ") + " = ";
+EXP_DIV.innerText = exp.join(" ");
 
 const USER_INPUT = document.getElementById("user-input");
-
-const GITHUB = document.createElement("a");
-GITHUB.innerText = "GitHub";
-GITHUB.style = "font-size: 0.8rem;font-weight: bold;position: absolute;top: 0px; width: 100%; color: white; text-align: center;"
-GITHUB.href = "https://github.com/aberrantian/mmt";
-GITHUB.referrerPolicy = "no-referrer";
-GITHUB.target = "_blank";
-document.body.append(GITHUB);
-
 
 function newExp() {
     console.clear();
@@ -31,8 +21,4 @@ function newExp() {
 
 document.body.addEventListener("keydown", (e) => {
     inputHandler(e);
-    
-    if (USER_INPUT.innerText === String(answer)) {
-        newExp();
-    }
 });
